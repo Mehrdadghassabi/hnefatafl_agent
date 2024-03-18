e0a1694c674de869468c04fee8e8203583b0376a
mehrdadghassabi <53050138+Mehrdadghassabi@users.noreply.github.com>
2024-03-18 04:55:38 +0330

    Add files via upload

--- Code/hnefatafl_train.py
+++ Code/hnefatafl_train.py
@@ -11,36 +11,91 @@ Date: 4/4/2019
 import sys
 import pygame
 from pygame.locals import *
-import time
 import random
 import numpy as np
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Activation, Dropout
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.models import load_model
 import hnefatafl as tafl
 import tools as tool
 
 
-def run_game_hahd(screen):
-    """ Start a human vs human game
-    """
-    # run_game(screen)
-    pass
+def Simple_heuristic(game_state, defender):
+    red_capture = 24 - np.count_nonzero(game_state == 1)
+    white_capture = 13 - np.count_nonzero(game_state == -1)
+    index = np.argwhere(game_state == -2)
+    x = index[0][0]
+    y = index[0][1]
+    distance_to_burg = min(x + y, 20 - x - y, x + 10 - y, y + 10 - x)
+    if defender:
+        return (red_capture - white_capture) + 0.1 * distance_to_burg
+    else:
+        return -((red_capture - white_capture) + 0.1 * distance_to_burg)
 
 
-def run_game_hacd(screen):
-    """ Start a human attacker vs computer defender game
-    """
-    # run_game(screen)
-    pass
+def Hingston_Simple_Agent(move, defender):
+    game_state = game_state_to_array()  # Preserves the current game state
 
+    if move.a_turn:
+        pieces = tafl.Attackers
+    else:
+        pieces = tafl.Defenders
 
-def run_game_cahd(screen):
-    """ Start a computer attacker vs human defender game
-    """
-    # run_game(screen)
-    pass
+    best_score = -99999999999999.0
+    best_piece = None
+    best_move = None
+    best_game_state = None
+    # len("N Pieces: ",len(pieces))
+    for piece in pieces:
+        move.select(piece)  # Move class defines all possible valid moves
+        tafl.Current.add(piece)
+        if len(move.vm) == 0:  # No valid moves for this piece, move on
+            move.select(piece)
+            tafl.Current.empty()
+            continue
+        else:
+            for m in move.vm:
+                # Swap game state for candidate move
+                temp = game_state[piece.x_tile][piece.y_tile]
+                game_state[piece.x_tile][piece.y_tile] = 0
+                game_state[m[0]][m[1]] = temp
+                score = Simple_heuristic(game_state, defender)
+                # print(game_state)
+                # print(Simple_heuristic(game_state))
+                # model.predict(game_state.reshape(1, 11 * 11))[0][0]
+
+                if score > best_score:
+                    best_score = score
+                    best_piece = piece
+                    best_move = m
+
+                # Reverse swap to restore game state
+                temp = game_state[m[0]][m[1]]
+                game_state[piece.x_tile][piece.y_tile] = temp
+                game_state[m[0]][m[1]] = 0
+        move.select(piece)  # Deselect
+        tafl.Current.empty()
+
+    move.select(best_piece)
+    tafl.Current.add(best_piece)
+    # print("Moving piece to: {}".format(pos))
+    if move.is_valid_move(best_move, tafl.Current.sprites()[0], True):
+        if tafl.Current.sprites()[0] in tafl.Kings:
+            move.king_escaped(tafl.Kings)
+        if move.a_turn:
+            move.remove_pieces(tafl.Defenders, tafl.Attackers, tafl.Kings)
+        else:
+            move.remove_pieces(tafl.Attackers, tafl.Defenders, tafl.Kings)
+        move.end_turn(tafl.Current.sprites()[0])
+        tafl.Current.empty()
+        # return best_score,game_state_to_array()
+        # Just do this by hand for efficiency
+        temp = game_state[best_piece.x_tile][best_piece.y_tile]
+        game_state[best_piece.x_tile][best_piece.y_tile] = 0
+        game_state[best_move[0]][best_move[1]] = temp
+
+        # print(game_state,best_score)
+        return game_state, best_score
+    else:
+        print("ERROR: Efficient move logic failed... Fix!")
+        sys.exit(1)
 
 
 def run_game_random(screen=None):
@@ -130,7 +185,7 @@ def do_random_move(move):
             break
 
 
-def run_game_cacd_RL(attacker_model, defender_model, screen=None):
+def run_game_cacd_RL(screen=None):
     """Start and run one game of computer vs computer hnefatafl.
 
     TODO: Add description
@@ -162,9 +217,10 @@ def run_game_cacd_RL(attacker_model, defender_model, screen=None):
 
         if move.a_turn:
             # print("Attacker's Turn: Move {}".format(num_moves))
-            #game_state, predicted_score = do_best_move(move, attacker_model)
-            game_state = do_random_move(move)
-            predicted_score = (random.random() - 0.5) * 2
+            # game_state, predicted_score = do_best_move(move, attacker_model)
+            game_state, predicted_score = Hingston_Simple_Agent(move, False)
+            # game_state = do_random_move(move)
+            # predicted_score = (random.random() - 0.5) * 2
             a_game_states.append(game_state)
             a_predicted_scores.append(predicted_score)
         else:
@@ -270,27 +326,6 @@ def do_best_move(move, model):
         sys.exit(1)
 
 
-def initialize_random_nn_model():
-    print("Initializing randomized NN model")
-    model = Sequential()
-    model.add(Dense(2 * 11 * 11, input_dim=11 * 11, kernel_initializer='normal', activation='relu'))
-    model.add(Dropout(0.1))
-    model.add(Dense(11 * 11, kernel_initializer='normal', activation='relu'))
-    model.add(Dropout(0.1))
-    # model.add(Dense(9, kernel_initializer='normal',activation='relu'))
-    # model.add(Dropout(0.1))
-    # model.add(Dense(5, kernel_initializer='normal',activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
-
-    learning_rate = 0.001
-    momentum = 0.8
-
-    sgd = SGD(lr=learning_rate, momentum=momentum, nesterov=False)
-    model.compile(loss='mean_squared_error', optimizer=sgd)
-    model.summary()
-    return model
-
-
 def game_state_to_array():
     """2D Numpy array representation of game state for ML model.
     """
@@ -302,9 +337,9 @@ def game_state_to_array():
     for p in tafl.Attackers:
         arr[p.x_tile][p.y_tile] = '1'
     for p in tafl.Defenders:
-        arr[p.x_tile][p.y_tile] = '2'
+        arr[p.x_tile][p.y_tile] = '-1'
     for p in tafl.Kings:
-        arr[p.x_tile][p.y_tile] = '3'
+        arr[p.x_tile][p.y_tile] = '-2'
 
     return arr
 
@@ -336,27 +371,29 @@ def main():
     tool.initialize_groups()
 
     # attacker_model = initialize_random_nn_model()
-    attacker_model = load_model('attacker_model_after_340_games.h5')
-    defender_model = initialize_random_nn_model()
+    # defender_model = initialize_random_nn_model()
 
     # train = True
-    num_train_games = 340
+    num_train_games = 0
     # while train:
-    while num_train_games < 10000:
+    while num_train_games < 10:
         num_train_games += 1
         # play = tafl.run_game(screen)
         # play = run_game_cacd(screen)
         # a_game_states,a_corrected_scores, d_game_states,d_corrected_scores = run_game_cacd_RL(attacker_model,defender_model)
-        a_game_states, a_corrected_scores, d_game_states, d_corrected_scores = run_game_cacd_RL(attacker_model,
-                                                                                                defender_model, screen)
+        a_game_states, a_corrected_scores, d_game_states, d_corrected_scores = run_game_cacd_RL(screen)
         # play = run_game_cacd_RL(attacker_model,defender_model,screen)
         print("Game finished in {} moves".format(len(a_corrected_scores) + len(d_corrected_scores)))
         # a_game_states,a_corrected_scores = unison_shuffled_copies(a_game_states,a_corrected_scores)
         smooth_corrected_scores(a_corrected_scores)
-        attacker_model.fit(np.array(a_game_states).reshape(-1, 11 * 11), np.array(a_corrected_scores), epochs=1,
-                           batch_size=1, verbose=0)
-        if num_train_games % 10 == 0:
-            attacker_model.save('attacker_model_after_{}_games.h5'.format(num_train_games))
+        # print(len(a_game_states))
+        # print(a_corrected_scores)
+        # print(d_game_states)
+        # print(d_corrected_scores)
+        #attacker_model.fit(np.array(a_game_states).reshape(-1, 11 * 11), np.array(a_corrected_scores), epochs=1,
+                           #batch_size=1, verbose=0)
+        #if num_train_games % 10 == 0:
+            #attacker_model.save('attacker_model_after_{}_games.h5'.format(num_train_games))
 
         # time.sleep(5)
         tool.cleanup()
@@ -364,23 +401,15 @@ def main():
 
 def main1():
     """Main function- initializes screen and starts new games."""
-    interactive = True
+    interactive = False
     if interactive:
         pygame.init()
         screen = pygame.display.set_mode(tafl.WINDOW_SIZE)
     else:
         screen = None
     tool.initialize_groups()
-
-    attacker_model = initialize_random_nn_model()
-    defender_model = initialize_random_nn_model()
-
-    a_game_states, a_corrected_scores, d_game_states, d_corrected_scores = run_game_cacd_RL(attacker_model,
-                                                                                            defender_model, screen)
-    # play = run_game_cacd_RL(attacker_model,defender_model,screen)
+    a_game_states, a_corrected_scores, d_game_states, d_corrected_scores = run_game_cacd_RL(screen)
     print("Game finished in {} moves".format(len(a_corrected_scores) + len(d_corrected_scores)))
-
-    # time.sleep(5)
     tool.cleanup()
 
 
